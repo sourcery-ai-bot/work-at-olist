3aae4b068beb841673ebfe3546f0d8470d2865c8
Luiz de Prá <luiz@smartgreen.net>
2016-12-02 17:53:41 -0200

    Add STATIC_ROOT to settings.py
    Also, it adds the generated 'static' folder to .gitgnore.

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -43,8 +43,7 @@ ROOT_URLCONF = 'workatolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
