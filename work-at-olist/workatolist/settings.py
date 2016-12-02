088f44bf6924ddb215f6380cde1134cf075029e1
Luiz de Prá <luiz@smartgreen.net>
2016-12-02 18:04:01 -0200

    Add ALLOWED_HOSTS

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = 'ld7s4bcrf)mso_*y^mnnfrr=fh&)4gs$*e%-acjxw5nyulpioz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'wao.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
