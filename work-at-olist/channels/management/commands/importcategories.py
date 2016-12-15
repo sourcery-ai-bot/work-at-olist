b4fd69d17a43c369909ce200854c59f20e17dab1
Luiz de Prá <rawarkanis@gmail.com>
2016-12-15 13:13:49 -0200

    Add importcategories command
    This commit adds the 'importcategories' command to the manage.py. It
    imports a list for categories from csv file.

---
+++ work-at-olist/channels/management/commands/importcategories.py
@@ -0,0 +1,75 @@
+"""A custom 'manage.py' command that imports categories from a CSV file."""
+
+import csv
+from django.db import transaction
+from django.core.management.base import BaseCommand, CommandError
+from channels.models import Channel, Category
+
+
+class Command(BaseCommand):
+    """Command that imports categories from a CSV file.
+
+    The 'importcategories' command creates a Channel if the provided name don't
+    exists. And then, it creates all the imported categories. If
+    """
+
+    help = 'Imports categories from a CSV file'
+
+    def add_arguments(self, parser):
+        """Adds 'channel' and 'csv' args to the command."""
+        parser.add_argument('channel', type=str)
+        parser.add_argument('csv', type=open)
+
+    def validate_ancestors(self, channel_ref, categories):
+        """Checks if all category ancestors are already registered and returns
+        the last one, the parent.
+        """
+        if not len(categories):
+            return None, None
+
+        parent_ref = Category.generate_reference(
+            categories[-1], [channel_ref] + categories[0:-1])
+        try:
+            parent = Category.objects.get(reference=parent_ref)
+        except Category.DoesNotExist:
+            return None, 'Parent does not exist'
+
+        ancestors = parent.get_ancestors()
+        if len(ancestors) != len(categories[0:-1]):
+            return None, 'Mismatching number of ancestors'
+        for index, value in enumerate(categories[0:-1]):
+            if value != ancestors[index].name:
+                return None, (
+                    'Mismatching ancestor at level {:d} ({:s} should be {:s})'
+                ).format(index, ancestors[index].name, value)
+
+        return parent, None
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        """Perform the operations of importcategories command."""
+        channel_ref = Channel.generate_reference(options['channel'])
+        channel, created = Channel.objects.get_or_create(
+            reference=channel_ref, defaults={'name': options['channel']})
+
+        if not created:
+            Category.objects.filter(channel=channel).delete()
+
+        reader = csv.DictReader(options['csv'])
+        for row in reader:
+            full_path = [x.strip() for x in row['Category'].split('/')]
+            ancestors = full_path[0:-1]
+            current = full_path[-1]
+
+            parent, error = self.validate_ancestors(channel_ref, ancestors)
+            if error:
+                raise CommandError('Unable to process "{:s}": {:s}'.format(row[
+                    'Category'], error))
+
+            category_ref = Category.generate_reference(
+                current, [channel_ref] + ancestors)
+            Category.objects.create(
+                reference=category_ref,
+                name=current,
+                channel=channel,
+                parent=parent)
---
+++ work-at-olist/channels/tests/test_importcategories.py
@@ -0,0 +1,93 @@
+"""ImportCategories command's test cases of the Channels app."""
+
+import csv
+import tempfile
+from django.test import TestCase
+from django.core.management import call_command
+from channels.models import Channel
+
+
+def create_tempcsv(content):
+    """Creates a temporary csv file and fill with 'content'. Returns its
+    'filename'.
+    """
+    csvfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+
+    fieldnames = ['Category']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for item in content:
+        writer.writerow({'Category': item})
+
+    filename = csvfile.name
+    csvfile.close()
+
+    return filename
+
+
+class ImportCategoriesTest(TestCase):
+    """ImportCategories Command's Test Cases."""
+
+    def test_empty(self):
+        """Tests importcategories command against a empty file."""
+        csvfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        filename = csvfile.name
+        csvfile.close()
+
+        args = ['empty', filename]
+        opts = {}
+        call_command('importcategories', *args, **opts)
+
+        channel = Channel.objects.get(reference='empty')
+        self.assertIsNotNone(channel)
+
+        self.assertEqual(channel.categories.count(), 0)
+
+    def test_one_category(self):
+        """Tests importcategories command against a file with only one
+        category.
+        """
+        content = ['Books']
+        filename = create_tempcsv(content)
+
+        args = ['onecategory', filename]
+        opts = {}
+        call_command('importcategories', *args, **opts)
+
+        channel = Channel.objects.get(reference='onecategory')
+        self.assertIsNotNone(channel)
+
+        categories = channel.categories.all()
+        self.assertEqual(categories.count(), 1)
+        self.assertEqual(categories.first().name, content[0])
+
+    def test_full_tree(self):
+        """Tests importcategories command against a file with a complete
+        category tree.
+        """
+        content = [
+            'Books', 'Books / National Literature',
+            'Books / National Literature / Science Fiction',
+            'Books / National Literature / Fiction Fantastic',
+            'Books / Foreign Literature', 'Books / Computers',
+            'Books / Computers / Applications', 'Books / Computers / Database',
+            'Books / Computers / Programming', 'Games', 'Games / XBOX 360',
+            'Games / XBOX 360 / Console', 'Games / XBOX 360 / Games',
+            'Games / XBOX 360 / Accessories', 'Games / XBOX One',
+            'Games / XBOX One / Console', 'Games / XBOX One / Games',
+            'Games / XBOX One / Accessories', 'Games / Playstation 4',
+            'Computers', 'Computers / Notebooks', 'Computers / Tablets',
+            'Computers / Desktop'
+        ]
+        filename = create_tempcsv(content)
+
+        args = ['onecategory', filename]
+        opts = {}
+        call_command('importcategories', *args, **opts)
+
+        channel = Channel.objects.get(reference='onecategory')
+        self.assertIsNotNone(channel)
+
+        categories = channel.categories.all()
+        self.assertEqual(categories.count(), len(content))
