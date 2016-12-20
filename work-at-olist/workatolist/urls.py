db58b31b832e040558f89f3b547fdb198b8f057a
Luiz de Prá <rawarkanis@gmail.com>
2016-12-20 14:04:36 -0200

    Add API version

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -3,4 +3,4 @@
 from django.conf.urls import url, include
 from channels import urls as channels_url
 
-urlpatterns = [url(r'^api/', include(channels_url, namespace='api'))]
+urlpatterns = [url(r'^api/v1/', include(channels_url, namespace='api'))]
