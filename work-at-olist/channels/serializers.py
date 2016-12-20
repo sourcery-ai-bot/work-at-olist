5eacde0deffd72e89051f9bc76032ca00c72d054
Luiz de Prá <rawarkanis@gmail.com>
2016-12-20 17:13:21 -0200

    Add more serializers and tests
    This commit adds some serializers for the new endpoints. Also, adds
    tests and its fixture.

--- work-at-olist/channels/serializers.py
+++ work-at-olist/channels/serializers.py
@@ -1,6 +1,7 @@
 """Serializers of the Channels' models."""
 
 from rest_framework import serializers
+from rest_framework_recursive.fields import RecursiveField
 from channels.models import Channel, Category
 
 
@@ -23,7 +24,7 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Category
-        fields = ('url', 'name', 'channel', 'parent')
+        fields = ('url', 'name', 'channel', 'parent', 'children')
         extra_kwargs = {
             'url': {
                 'view_name': 'api:category-detail',
@@ -35,6 +36,64 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
             },
             'parent': {
                 'view_name': 'api:category-detail',
+                'lookup_field': 'reference'
+            },
+            'children': {
+                'view_name': 'api:category-detail',
+                'lookup_field': 'reference'
+            }
+        }
+
+
+class ParentCategorySerializer(serializers.HyperlinkedModelSerializer):
+    """Parent Category Serializer used in the '/categories/relatives' endpoint.
+    """
+    parent = RecursiveField(allow_null=True)
+
+    class Meta:
+        model = Category
+        fields = ('url', 'name', 'parent')
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:category-detail',
+                'lookup_field': 'reference'
+            }
+        }
+
+
+class ChildrenCategorySerializer(serializers.HyperlinkedModelSerializer):
+    """Children Category Serializer used in the '/categories/relatives'
+    endpoint."""
+    children = RecursiveField(required=False, allow_null=True, many=True)
+
+    class Meta:
+        model = Category
+        fields = ('url', 'name', 'children')
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:category-detail',
+                'lookup_field': 'reference'
+            }
+        }
+
+
+class CategoryRelativesSerializer(serializers.HyperlinkedModelSerializer):
+    """Category Serializer used in the '/categories/relatives' endpoint. It
+    uses Hyperlinked relation for the related Channel and nests parents and children
+    Categories."""
+    parent = ParentCategorySerializer()
+    children = ChildrenCategorySerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ('url', 'name', 'channel', 'parent', 'children')
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:category-detail',
+                'lookup_field': 'reference'
+            },
+            'channel': {
+                'view_name': 'api:channel-detail',
                 'lookup_field': 'reference'
             }
         }
--- work-at-olist/channels/tests/test_serializers.py
+++ work-at-olist/channels/tests/test_serializers.py
@@ -1,53 +1,218 @@
 """Serializers' test cases."""
 
 from django.test import TestCase
-from channels.models import Channel, Category
-from channels.serializers import ChannelSerializer, CategorySerializer
+from channels import models, serializers
 
 
 class ChannelSerializerTest(TestCase):
     """ChannelSerializer's Test Cases."""
 
-    def test_empty(self):
-        """Tests creation of a empty ChannelSerializer."""
-        expected_data = {'name': ''}
-
-        serializer = ChannelSerializer()
-
-        self.assertEqual(serializer.data, expected_data)
+    fixtures = ['serializerstests']
 
     def test_create(self):
         """Tests ChannelSerializer's creation."""
-        expected_data = {'url': '/api/channels/test/', 'name': 'Test'}
+        expected_data = {'url': '/api/v1/channels/test/', 'name': 'Test'}
 
-        model = Channel(reference='test', name='Test')
-        serializer = ChannelSerializer(model, context={'request': None})
+        model = models.Channel.objects.get(reference='test')
+        serializer = serializers.ChannelSerializer(
+            model, context={'request': None})
 
         self.assertEqual(serializer.data, expected_data)
+
+    def test_validate(self):
+        """Tests ChannelSerializer's validation."""
+        data = {'name': 'Channel'}
+
+        serializer = serializers.ChannelSerializer(
+            data=data)
+
+        self.assertTrue(serializer.is_valid())
 
 
 class CategorySerializerTest(TestCase):
     """CategorySerializer's Test Cases."""
 
-    def test_empty(self):
-        """Tests creation of a empty CategorySerializer."""
-        expected_data = {'name': '', 'channel': None, 'parent': None}
-
-        serializer = CategorySerializer()
-
-        self.assertEqual(serializer.data, expected_data)
+    fixtures = ['serializerstests']
 
     def test_create(self):
         """Tests CategorySerializer's creation."""
         expected_data = {
-            'url': '/api/categories/test/',
-            'name': 'Test',
-            'channel': '/api/channels/channel/',
-            'parent': None
+            'url': '/api/v1/categories/test-root-one-two/',
+            'name': 'Two',
+            'channel': '/api/v1/channels/test/',
+            'parent': '/api/v1/categories/test-root-one/',
+            'children': []
         }
 
-        channel = Channel(reference='channel', name='Channel')
-        category = Category(reference='test', name='Test', channel=channel)
-        serializer = CategorySerializer(category, context={'request': None})
+        category = models.Category.objects.get(reference='test-root-one-two')
+        serializer = serializers.CategorySerializer(
+            category, context={'request': None})
 
         self.assertEqual(serializer.data, expected_data)
+
+    def test_validate(self):
+        """Tests CategorySerializer's validation."""
+        data = {
+            'name': 'Category',
+            'channel': '/api/v1/channels/test/',
+            'parent': '/api/v1/categories/test-root/',
+            'children': []
+        }
+
+        serializer = serializers.CategorySerializer(
+            data=data)
+
+        self.assertTrue(serializer.is_valid())
+
+
+class ParentCategorySerializer(TestCase):
+    """ParentCategorySerializer's Test Cases."""
+
+    fixtures = ['serializerstests']
+
+    def test_create(self):
+        """Tests ParentCategorySerializer's creation."""
+        expected_data = {
+            'url': '/api/v1/categories/test-root-one-two/',
+            'name': 'Two',
+            'parent': {
+                'url': '/api/v1/categories/test-root-one/',
+                'name': 'One',
+                'parent': {
+                    'url': '/api/v1/categories/test-root/',
+                    'name': 'Root',
+                    'parent': None
+                }
+            }
+        }
+
+        category = models.Category.objects.get(reference='test-root-one-two')
+
+        serializer = serializers.ParentCategorySerializer(
+            category, context={'request': None})
+
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_validate(self):
+        """Tests ParentCategorySerializer's validation."""
+        data = {
+            'name': 'Category',
+            'parent': {
+                'name': 'Category',
+                'parent': None
+            }
+        }
+
+        serializer = serializers.ParentCategorySerializer(
+            data=data)
+
+        self.assertTrue(serializer.is_valid())
+
+
+class ChildrenCategorySerializer(TestCase):
+    """ChildrenCategorySerializer's Test Cases."""
+
+    fixtures = ['serializerstests']
+
+    def test_create(self):
+        """Tests ChildrenCategorySerializer's creation."""
+        expected_data = {
+            'url': '/api/v1/categories/test-root/',
+            'name': 'Root',
+            'children': [{
+                'url': '/api/v1/categories/test-root-one/',
+                'name': 'One',
+                'children': [{
+                    'url': '/api/v1/categories/test-root-one-two/',
+                    'name': 'Two',
+                    'children': []
+                }, {
+                    'url': '/api/v1/categories/test-root-one-two-two/',
+                    'name': 'Two Two',
+                    'children': []
+                }]
+            }]
+        }
+
+        category = models.Category.objects.get(reference='test-root')
+
+        serializer = serializers.ChildrenCategorySerializer(
+            category, context={'request': None})
+
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_validate(self):
+        """Tests ChildrenCategorySerializer's validation."""
+        data = {
+            'name': 'Category',
+            'children': [{
+                'name': 'One',
+                'children': []
+            }, {
+                'name': 'Two',
+                'children': []
+            }]
+        }
+
+        serializer = serializers.ChildrenCategorySerializer(
+            data=data)
+
+        self.assertTrue(serializer.is_valid())
+
+
+class CategoryRelativesSerializer(TestCase):
+    """CategoryRelativesSerializer's Test Cases."""
+
+    fixtures = ['serializerstests']
+
+    def test_create(self):
+        """Tests CategoryRelativesSerializer's creation."""
+        expected_data = {
+            'url': '/api/v1/categories/test-root-one/',
+            'name': 'One',
+            'channel': '/api/v1/channels/test/',
+            'parent': {
+                'url': '/api/v1/categories/test-root/',
+                'name': 'Root',
+                'parent': None
+            },
+            'children': [{
+                'url': '/api/v1/categories/test-root-one-two/',
+                'name': 'Two',
+                'children': []
+            }, {
+                'url': '/api/v1/categories/test-root-one-two-two/',
+                'name': 'Two Two',
+                'children': []
+            }]
+        }
+
+        category = models.Category.objects.get(reference='test-root-one')
+
+        serializer = serializers.CategoryRelativesSerializer(
+            category, context={'request': None})
+
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_validate(self):
+        """Tests CategoryRelativesSerializer's validation."""
+        data = {
+            'name': 'Category',
+            'channel': '/api/v1/channels/test/',
+            'parent': {
+                'name': 'Category',
+                'parent': None
+            },
+            'children': [{
+                'name': 'One',
+                'children': []
+            }, {
+                'name': 'Two',
+                'children': []
+            }]
+        }
+
+        serializer = serializers.CategoryRelativesSerializer(
+            data=data)
+
+        self.assertTrue(serializer.is_valid())
