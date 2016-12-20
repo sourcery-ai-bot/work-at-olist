e29190a3a79dc337c928690b1107d421de05523e
Luiz de Prá <rawarkanis@gmail.com>
2016-12-20 14:13:14 -0200

    Add Category nested route

--- work-at-olist/channels/urls.py
+++ work-at-olist/channels/urls.py
@@ -1,10 +1,18 @@
 """URLs from Channels app."""
 
+from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from channels.views import ChannelViewSet, CategoryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'channels', ChannelViewSet)
 router.register(r'categories', CategoryViewSet)
 
-urlpatterns = router.urls
+channel_router = nested_routers.NestedSimpleRouter(
+    router, r'channels', lookup='channels')
+channel_router.register(r'categories', CategoryViewSet)
+
+urlpatterns = [
+    url(r'', include(router.urls)), url(r'', include(channel_router.urls))
+]
