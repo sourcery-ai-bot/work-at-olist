c65e5663ebb76ced7b29190bc461e5219f79f8b4
Luiz de Prá <rawarkanis@gmail.com>
2016-12-22 16:24:26 -0200

    Adding /categories/{ref}/relatives/ endpoint
    This commit adds the endpoint that retrieves a category with its parents
    and children. Also, it add views' search and ordering configuration, and
    more view tests.

--- work-at-olist/channels/tests/test_views.py
+++ work-at-olist/channels/tests/test_views.py
@@ -5,50 +5,135 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 
-class ChannelViewSetTest(TestCase):
-    """ChannelViewSet's Test Cases."""
+class BaseViewSetTest(TestCase):
+    """Base class for ViewSet test cases."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+
+class ChannelViewSetTest(BaseViewSetTest):
+    """ChannelViewSet's test cases."""
 
     fixtures = ['viewtests']
 
     def test_list(self):
         """Tests '/api/v1/channels/' endpoint."""
-        client = APIClient()
-
-        response = client.get('/api/v1/channels/', format='json')
+        response = self.client.get('/api/v1/channels/', format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
 
     def test_retrieve(self):
         """Tests '/api/v1/channels/{reference}' endpoint."""
-        client = APIClient()
-
-        response = client.get('/api/v1/channels/walmart/', format='json')
+        response = self.client.get('/api/v1/channels/walmart/', format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], 'walmart')
 
+    def test_retrieve_non(self):
+        """Tests '/api/v1/channels/{reference}' endpoint against a nonexistent
+        reference."""
+        response = self.client.get('/api/v1/channels/amazon/', format='json')
 
-class CategoryViewSetTest(TestCase):
-    """CategoryViewSet's Test Cases."""
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CategoryViewSetTest(BaseViewSetTest):
+    """CategoryViewSet's test cases."""
 
     fixtures = ['viewtests']
 
     def test_list(self):
         """Tests '/api/v1/categories/' endpoint."""
-        client = APIClient()
-
-        response = client.get('/api/v1/categories/', format='json')
+        response = self.client.get('/api/v1/categories/', format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 24)
 
     def test_retrieve(self):
         """Tests '/api/v1/categories/{reference}' endpoint."""
-        client = APIClient()
-
-        response = client.get(
+        response = self.client.get(
             '/api/v1/categories/walmart-games-xbox-one-games/', format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], 'Games')
+
+    def test_retrieve_non(self):
+        """Tests '/api/v1/categories/{reference}' endpoint against a
+        nonexistent reference."""
+        response = self.client.get('/api/v1/categories/amazon-books/',
+                                   format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_relatives(self):
+        """Tests 'api/v1/categories/{reference}/relatives/ endpoint."""
+        response = self.client.get(
+            '/api/v1/categories/walmart-games-xbox-one/relatives/',
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'XBOX One')
+
+    def test_relatives_non(self):
+        """Tests 'api/v1/categories/{reference}/relatives/ endpoint against a
+        nonexistent reference."""
+        response = self.client.get(
+            '/api/v1/categories/amazon-books/relatives/', format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_nested_list(self):
+        """Tests '/api/v1/channels/{reference}/categories/' endpoint."""
+        response = self.client.get('/api/v1/channels/walmart/categories/',
+                                   format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 24)
+
+    def test_nested_list_non(self):
+        """Tests '/api/v1/channels/{reference}/categories/' endpoint against a
+        nonexistent channel's reference."""
+        response = self.client.get('/api/v1/channels/amazon/categories/',
+                                   format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 0)
+
+    def test_nested_retrieve(self):
+        """Tests '/api/v1/channels/{reference}/categories/{reference}'
+        endpoint."""
+        response = self.client.get(
+            '/api/v1/channels/walmart/categories/walmart-games-xbox-one-games/',
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Games')
+
+    def test_nested_retrieve_non(self):
+        """Tests '/api/v1/channels/{reference}/categories/{reference}'
+        endpoint against a nonexistent channel's reference."""
+        response = self.client.get(
+            '/api/v1/channels/amazon/categories/walmart-games/', format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_nested_relatives(self):
+        """Tests 'api/v1/channels/{reference}/categories/{reference}/relatives/
+        endpoint."""
+        response = self.client.get(
+            '/api/v1/channels/walmart/categories/walmart-games-xbox-one/relatives/',
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'XBOX One')
+
+    def test_nested_relatives_non(self):
+        """Tests 'api/v1/channels/{reference}/categories/{reference}/relatives/
+        endpoint against a nonexistent channel's reference."""
+        response = self.client.get(
+            '/api/v1/channels/amazon/categories/walmart-games/relatives/',
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -3,29 +3,43 @@
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from channels.models import Channel, Category
-from channels.serializers import ChannelSerializer, CategorySerializer
+from channels import models, serializers
 
 
 class ChannelViewSet(viewsets.ReadOnlyModelViewSet):
     """Channel's API ViewSet with list and detail endpoints."""
 
-    queryset = Channel.objects.all()
-    serializer_class = ChannelSerializer
+    queryset = models.Channel.objects.all()
+    serializer_class = serializers.ChannelSerializer
     lookup_field = 'reference'
-
-    @detail_route(methods=['get'])
-    def categories(self, request, reference):
-        """Endpoint that lists all categories of a Channel."""
-        channel = self.get_object()
-        serializer = CategorySerializer(
-            channel.categories.all(), context={'request': request}, many=True)
-        return Response(serializer.data)
+    search_fields = ('reference', 'name')
+    ordering_fields = ('reference', 'name')
+    ordering = ('reference', )
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """Category's API ViewSet with list and detail endpoints."""
 
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
     lookup_field = 'reference'
+    search_fields = ('reference', 'name', 'channel__reference',
+                     'channel__name')
+    ordering_fields = ('reference', 'name', 'channel__reference',
+                       'channel__name')
+    ordering = ('channel__reference', 'reference')
+
+    def get_queryset(self):
+        """Retrieves the queryset using the 'channel_pk' if present."""
+        if 'channels_reference' in self.kwargs:
+            return self.queryset.filter(
+                channel__reference=self.kwargs['channels_reference'])
+        return self.queryset
+
+    @detail_route(methods=['get'])
+    def relatives(self, request, reference, channels_reference=None):
+        """Endpoint that retrieves a category with its ancestors and chidren."""
+        category = self.get_object()
+        serializer = serializers.CategoryRelativesSerializer(
+            category, context={'request': request})
+        return Response(serializer.data)
